9bfcbcbf3b59208e45b5e18426964e591b92ef21
Leibniz Li <stormte@gmail.com>
2024-04-03 13:10:13 +0800

    popstate

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,18 @@
-const avatar = document.querySelector('img.avatar.avatar-user.width-full');
-if (avatar) {
-  chrome.storage.sync.get(
-    {borderRadius: '0.375rem'},
-    (items) => {
-      avatar.style.setProperty('border-radius', items.borderRadius, 'important');
-    }
-  );
-
+function main(){
+  const avatar = document.querySelector('img.avatar.avatar-user.width-full');
+  console.log(avatar);
+  if (avatar) {
+    chrome.storage.sync.get(
+      {borderRadius: '0.375rem'},
+      (items) => {
+        avatar.style.setProperty('border-radius', items.borderRadius, 'important');
+        avatar.style.setProperty('transition', '0.2s');
+      }
+    );
+  }
 }
+console.log(window.location.href);
+window.addEventListener('popstate', function(event) {
+  main();
+});
+main();
