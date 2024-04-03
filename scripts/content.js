015e22c3eb70750b94aa357e7e8bac9b52ff1bfb
Leibniz Li <stormte@gmail.com>
2024-04-03 20:16:29 +0800

    setInterval

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,22 @@
-function main(){
+let radiusValue = '0.375rem';
+chrome.storage.sync.get({borderRadius: '0.375rem'}, (items) => {
+    radiusValue = items.borderRadius;
+  }
+)
+
+function main() {
   const avatar = document.querySelector('img.avatar.avatar-user.width-full');
-  console.log(avatar);
   if (avatar) {
-    chrome.storage.sync.get(
-      {borderRadius: '0.375rem'},
-      (items) => {
-        avatar.style.setProperty('border-radius', items.borderRadius, 'important');
-        avatar.style.setProperty('transition', '0.2s');
-      }
-    );
+    avatar.style.setProperty('border-radius', radiusValue, 'important');
+    avatar.style.setProperty('transition', '0.2s');
   }
 }
-console.log(window.location.href);
-window.addEventListener('popstate', function(event) {
+
+main();
+
+setInterval(() => {
+  main();
+}, 3000);
+window.addEventListener('popstate', function (event) {
   main();
 });
-main();
